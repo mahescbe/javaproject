ba2058dd11f3f9017d95516f64f081e9bcbb2972
mahescbe <mahescbe@gmail.com>
2019-09-17 12:53:33 +0530

    Update App.java

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World  Test Test Test" );
+        System.out.println( "Hello World  Test Test Test test Test" );
     }
 
 	public int calculateSomething() {
